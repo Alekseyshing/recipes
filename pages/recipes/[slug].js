ffa8de1f064f50e57f51ca66e5835a9443220aa3
Aleksey Shingarev <alekseytim@mail.ru>
2022-12-26 21:39:44 +0300

    feat: 6

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,3 +1,10 @@
+import { createClient } from 'contentful'
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+})
+
 export default function RecipeDetails() {
   return (
     <div>
