142dc397c956f866ee73d1d8168af979e12ea490
Aleksey Shingarev <alekseytim@mail.ru>
2022-12-26 22:10:25 +0300

    feat: 8

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -5,7 +5,36 @@ const client = createClient({
   accessToken: process.env.CONTENTFUL_ACCESS_KEY,
 })
 
-export default function RecipeDetails() {
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: 'recipe'
+  })
+
+  const paths = res.items.map(item => {
+    return {
+      params: { slug: item.fields.slug }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: 'recipe',
+    'fields.slug': params.slug
+  })
+
+  return {
+    props: { recipe: items[0] }
+  }
+}
+
+export default function RecipeDetails({ recipe }) {
+  console.log(recipe);
   return (
     <div>
       Recipe Details
